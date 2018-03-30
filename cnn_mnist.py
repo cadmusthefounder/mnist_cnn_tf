ccd3b01916ef169f3b54d401c18c9d59d7c6144b
Charlton Lim <cadmusthefounder@gmail.com>
2018-03-30 16:06:36 +0800

    Create skeleton for main cnn file.

---
+++ cnn_mnist.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# Imports
+import numpy as np
+import tensorflow as tf
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+# Our application logic will be added here
+
+def main(argv):
+  print("Running main program.")
+
+if __name__ == "__main__":
+  tf.app.run()
